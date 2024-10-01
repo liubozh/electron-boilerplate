affcfa32d0be0dee4bd703232ab599167a345907
liubozh <bo.liu@hey.com>
2024-10-01 18:38:28 +0800

    add tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,6 @@
+import './index.css';
 import { createRoot } from 'react-dom/client';
-
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
-root.render(<h2>Hello from React!</h2>);
+root.render(<h1 className="text-3xl font-bold underline">Hello from React!</h1>);
